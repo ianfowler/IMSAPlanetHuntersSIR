f5bdd3cd3c8f8f8ee6e301b89c2e9bf8bd82a664
ianfowler <bearacuda13@gmail.com>
2019-10-09 10:44:11 -0500

    Made a working histogram!

--- Histogram.py
+++ Histogram.py
@@ -3,13 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 temp = pd.read_csv('KeplerFinal.txt')
-temp = temp.dropna()
+temp = temp.dropna().sort_values(by=['Teff'])
+data = list(temp['Teff'])
+
+binTempArr = [data[0]]
+
+lastTemp = 0
+numIndicies = 0
+
+minTempDifference = 100
+minNumIndicies = 100
 
 
-plt.hist(temp['Teff'], bins=400)
+for i in range(0,len(data)):
+	numIndicies += 1
+	if data[i] - lastTemp >= minTempDifference and numIndicies >= minNumIndicies:
+		lastTemp = data[i]
+		binTempArr.append(lastTemp)
+		numIndicies = 0
+
+plt.hist(data, edgecolor='white', bins=binTempArr)
 plt.show()
-
-
-
 
 
